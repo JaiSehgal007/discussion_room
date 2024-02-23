9f5e1005a1c22fbe299965610e2958f3310168d2
JaiSehgal007 <jaisehgal511@gmail.com>
2024-02-23 17:33:05 +0530

    Added CRUD routes to the Room Object

--- base/models.py
+++ base/models.py
@@ -18,6 +18,9 @@ class Room(models.Model):
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True) # only take a timstamp when it is crated
 
+    class Meta:
+        ordering=['-updated','-created']
+
     def __str__(self):
         return self.name
     
