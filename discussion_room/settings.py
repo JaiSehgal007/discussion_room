4ffbb2eba86daef345d8e33f04ef9a1387d7332c
JaiSehgal007 <jaisehgal511@gmail.com>
2024-02-25 00:27:16 +0530

    Configured Static Files

--- discussion_room/settings.py
+++ discussion_room/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# when the user inputs the profile picture
+# STATIC_ROOT=
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
